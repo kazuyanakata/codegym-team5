c683998f8727b3f8875696c671687b5e19906245
kazuyanakata <68310528+kazuyanakata@users.noreply.github.com>
2021-03-15 19:53:42 +0900

    テスト④

--- html/mycakeapp/src/Controller/AppController.php
+++ html/mycakeapp/src/Controller/AppController.php
@@ -72,7 +72,6 @@ class AppController extends Controller
     }
     public function beforeFilter(Event $event)
     {
-        $this->Security->blackHoleCallback = 'forceSSL';
         $this->Security->requireSecure();
         $this->Auth->allow();
         // 座席予約実装後はコメントを外す
@@ -84,8 +83,5 @@ class AppController extends Controller
             return true;
         }
         return false;
-    }
-    public function forceSSL() {
-        return $this->redirect('https://' . env('SERVER_NAME') . $this->here);
     }
 }
